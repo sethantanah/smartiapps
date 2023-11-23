495324e561c5473e191eac5f1446a6d04e460225
Seth Antanah <sethsyd32@outlook.com>
2023-11-23 08:55:41 +0000

    Disable file preview

--- fileserver/library/static/pdf_scripts.js
+++ fileserver/library/static/pdf_scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
     var adobeDCView = new AdobeDC.View({clientId: "c2190c7ef509475e8d332b7ec0097cb0", divId: "adobe-dc-view"});
     adobeDCView.previewFile({
-        content:  {location: {url: pdf_url}},
+        content:  {location: {url: pdf_url2}},
         metaData:{fileName: title}
     }, {showDownloadPDF: false, showPrintPDF: false});
 
